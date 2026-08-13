4a1c1c7dc2c5b300e28aa7ef6e086ef9f69deaaf
NoamTs123 <noamts09@gmail.com>
2026-08-13 14:36:09 +0300

    Update 1(concept of movement + start pos + map)

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello")
+import pygame
+import consts
+import game_field
+import solider
+
+
+GameField = game_field.CreateGameField(consts.RowAmount, consts.CallumnAmount)
+solider.StartSoldierPos(GameField, consts.SoldierWidth, consts.SoldierHeight)
+
+def HandleInput(events):
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    solider.MoovementUP(GameField)
+                elif event.key == pygame.K_DOWN:
+                    solider.MoovementDown(GameField)
+                elif event.key == pygame.K_LEFT:
+                    solider.MoovementLeft(GameField)
+                elif event.key == pygame.K_RIGHT:
+                    solider.MoovementRight(GameField)
+
+print(GameField)
