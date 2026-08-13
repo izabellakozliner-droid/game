bcfe1d28d69ea57f0cca76280500606aa0ee1089
NoamTs123 <noamts09@gmail.com>
2026-08-13 15:45:50 +0300

    Fixed bug with matrix forformation

--- game_field.py
+++ game_field.py
@@ -1,1 +1,18 @@
+import pygame
+import random
+import consts
+import numpy as np
 
+# def CreateGameField(row, calumn):
+#     GameField = []
+#     for i in range(calumn):
+#         GameField.append([])
+#         for j in range(calumn):
+#             GameField.append([])
+#     return GameField
+
+def CreateGameField(row, calumn):
+    GameField = [[0 for _ in range(calumn)] for _ in range(row)]
+    return GameField
+
+print(CreateGameField(consts.RowAmount, consts.CallumnAmount))
