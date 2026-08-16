4754e251ed7932b1850a2b4ce56826b281cc0268
shiraohana2008 <shiraohana2008@gmail.com>
2026-08-16 15:59:30 +0300

    e

--- screen.py
+++ screen.py
@@ -1,17 +1,32 @@
 import pygame
+import random
 import consts
 pygame.init()
-
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+def flagSpawn():
+    FLAG = pygame.image.load("flag.png").convert_alpha()
+    scaled_flag = pygame.transform.scale(FLAG, (consts.FLAG_HEIGHT,consts.FLAG_WIDTH))
+    screen.blit(scaled_flag, (60, 80))
+
+def solider_image():
+    SOLIDER = pygame.image.load('soldier.png')
+    scaled_flag = pygame.transform.scale(SOLIDER, (consts.PLAYER_WIDTH, consts. PLAYER_HEIGHT))
+    screen.blit(scaled_flag, (60,80))
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(consts.BORDER_COLOR)
+    solider_image()
+    flagSpawn()
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
+
+
+
