08c1f78b97b9b05b743e91e841451b3c8cc369d4
NoamTs123 <noamts09@gmail.com>
2026-08-16 15:45:34 +0300

    Trying to make this code to work again

--- game_field.py
+++ game_field.py
@@ -3,27 +3,51 @@ import random
 import consts
 import numpy as np
 
-# def CreateGameField(row, calumn):
-#     GameField = []
-#     for i in range(calumn):
-#         GameField.append([])
-#         for j in range(calumn):
-#             GameField.append([])
-#     return GameField
+import solider
 
-# def CreateGameField(row, calumn):
-#     GameField = [[0 for _ in range(calumn)] for _ in range(row)]
-#     return GameField
-#
-# print(CreateGameField(consts.RowAmount, consts.CallumnAmount))
+
+# from main import GameField
+
+def CreateGameField(row, calumn):
+    GameField = [[0 for _ in range(calumn)] for _ in range(row)]
+    return GameField
+
+print(CreateGameField(consts.RowAmount, consts.CallumnAmount))
 
 # def random_grass():
 #     grass_image = pygame.image.load("grass.png")
 #
 #     grass_display = grass_image[random.randint(0,20)]
 
-game_field = []
-def create_matrix():
-    global game_field
-    game_field = [50*[i for i in " "] for j in range(25)]
-    return game_field
+
+
+def FlagSpawn(GameField):
+    GameField[24][49] = 4
+    GameField[24][48] = 4
+    GameField[24][47] = 4
+    GameField[23][49] = 4
+    GameField[23][48] = 4
+    GameField[23][47] = 4
+    GameField[22][49] = 4
+    GameField[22][48] = 4
+    GameField[22][47] = 4
+    GameField[21][49] = 4
+    GameField[21][48] = 4
+    GameField[21][47] = 4
+    return GameField
+
+def MinesGenerator(GameField):
+    minesLeft = consts.MinesAmmount
+    while minesLeft > 0:
+        mineStartRow = random.randint(0, consts.RowAmount - 1)
+        mineStartCol = random.randint(0, consts.CallumnAmount - 1)
+        try:
+            if GameField[mineStartRow][mineStartCol] == 0 and GameField[mineStartRow][mineStartCol + 3] == 0 and mineStartCol < 48:
+                for i in range (3):
+                    GameField[mineStartRow][mineStartCol + i - 1] = -2
+                minesLeft -= 1
+        except IndexError:
+            pass
+    return GameField
+
+# print(CreateGameField(25, 50))
