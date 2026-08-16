0af8398abf80921cc082343b61a63b87bb26c185
jbt <shacharab18@gmail.com>
2026-08-16 12:00:55 +0300

    מטריצה ראשית

--- game_field.py
+++ game_field.py
@@ -11,8 +11,19 @@ import numpy as np
 #             GameField.append([])
 #     return GameField
 
-def CreateGameField(row, calumn):
-    GameField = [[0 for _ in range(calumn)] for _ in range(row)]
-    return GameField
+# def CreateGameField(row, calumn):
+#     GameField = [[0 for _ in range(calumn)] for _ in range(row)]
+#     return GameField
+#
+# print(CreateGameField(consts.RowAmount, consts.CallumnAmount))
 
-print(CreateGameField(consts.RowAmount, consts.CallumnAmount))
+# def random_grass():
+#     grass_image = pygame.image.load("grass.png")
+#
+#     grass_display = grass_image[random.randint(0,20)]
+
+game_field = []
+def create_matrix():
+    global game_field
+    game_field = [50*[i for i in " "] for j in range(25)]
+    return game_field
