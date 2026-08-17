b12b1c562b3302efea0bfaa74696ba2d5ce1c9bf
NoamTs123 <noamts09@gmail.com>
2026-08-17 14:03:59 +0300

    Movement FINALY works

--- solider.py
+++ solider.py
@@ -2,71 +2,58 @@ import consts
 import random
 import pygame
 import game_field
+import main
+soldier_pos = {"row": 0, "col": 0}
 
-def StartSoldierPos(Player_matrix):
-    Player_matrix[0][0] = 1
-    Player_matrix[0][1] = 1
-    Player_matrix[1][0] = 1
-    Player_matrix[1][1] = 1
-    Player_matrix[2][0] = 1
-    Player_matrix[2][1] = 1
-    Player_matrix[3][0] = 1
-    Player_matrix[3][1] = 1
-    return Player_matrix
+
+
+def ClearSoldier(Player_matrix):
+    for r in range(soldier_pos["row"], soldier_pos["row"] + consts.PLAYER_HEIGHT):
+        for c in range(soldier_pos["col"], soldier_pos["col"] + consts.PLAYER_WIDTH):
+            if (0 <= r < len(Player_matrix) and 0 <= c < len(Player_matrix[0]) and Player_matrix[r][c] == 1):
+                Player_matrix[r][c] = "_"
+
+def DrawSoldier(Player_matrix):
+    for r in range(soldier_pos["row"], soldier_pos["row"] + consts.PLAYER_HEIGHT):
+        for c in range(soldier_pos["col"], soldier_pos["col"] + consts.PLAYER_WIDTH):
+            if 0 <= r < len(Player_matrix) and 0 <= c < len(Player_matrix[0]):
+                Player_matrix[r][c] = 1
+    for col in zip(Player_matrix):
+        print(list(col))
+
+
 
 def MoovementUP(Player_matrix):
-    for j in range(len(Player_matrix[0])):#בודק שורה ראשונה במטריצה
-        if Player_matrix[0][j] == (1):
-            #1 בודק אם החייל במקום ריק
-            #-1 בודק אם החייל על פצצה
-            #5 בודק אם החייל הגיע לדגל
-            return
+    if soldier_pos["row"] > 0:
+        ClearSoldier(Player_matrix)
+        soldier_pos["row"] -= 1
+        DrawSoldier(Player_matrix)
+        main.PlayerPos.y -= 25
 
-    for i in range(len(Player_matrix)):#בודק כל שורה בלוח (אינדקס)
-        for j in range(len(Player_matrix[i])):#בודק כל עמודה בשורה(אינדקס)
-            if Player_matrix[i][j] == 1:#בודק אם המקום בלוח שווה 1 כלומר החייל
-                Player_matrix[i-1][j] += 1#זז שורה למעלה ונשאר באותה עמודה
-                Player_matrix[i][j] -= 1#המקום שהחייל היה הופך לריק
 
 
 def MoovementDown(Player_matrix):
+    if soldier_pos["row"] + consts.PLAYER_HEIGHT < 24:
+        ClearSoldier(Player_matrix)
+        soldier_pos["row"] += 1
+        DrawSoldier(Player_matrix)
+        main.PlayerPos.y += 25
 
-    for j in range(len(Player_matrix[24])):#בודק שורה אררונה במטריצה
-        if Player_matrix[0][j] == (1):
-            # 1 בודק אם החייל במקום ריק
-            # -1 בודק אם החייל על פצצה
-            # 5 בודק אם החייל הגיע לדגל
-            return
-
-    for i in range(len(Player_matrix)):#בודק כל שורה בלוח (אינדקס)
-        for j in range(len(Player_matrix[i])):#בודק כל עמודה בשורה(אינדקס)
-            if Player_matrix[i][j] == 1:#בודק אם המקום בלוח שווה 1 כלומר החייל
-                Player_matrix[i+1][j] += 1#זז שורה למטה ונשאר באותה עמודה
-                Player_matrix[i][j] -= 1#המקום שהחייל היה הופך לריק
 
 def MoovementRight(Player_matrix):
-
-    for i in range(len(Player_matrix)):
-        if Player_matrix[i][24] == (1):
-            return
-
-    for i in range(len(Player_matrix)):
-        for j in range(len(Player_matrix[i]) - 1, -1, -1):
-            if Player_matrix[i][j] == 1:
-                Player_matrix[i][j+1] += 1
-                Player_matrix[i][j] -= 1
+    if soldier_pos["col"] + consts.PLAYER_WIDTH < 49:
+        ClearSoldier(Player_matrix)
+        soldier_pos["col"] += 1
+        DrawSoldier(Player_matrix)
+        main.PlayerPos.x += 25
 
 def MoovementLeft(Player_matrix):
+    if soldier_pos["col"] > 0:
+        ClearSoldier(Player_matrix)
+        soldier_pos["col"] -= 1
+        DrawSoldier(Player_matrix)
+        main.PlayerPos.x -= 25
 
-    for i in range(len(Player_matrix)):
-        if Player_matrix[i][0] == (1):
-            return
-
-    for i in range(len(Player_matrix)):
-        for j in range(len(Player_matrix[i])):
-            if Player_matrix[i][j] == 1:
-                Player_matrix[i][j-1] += 1
-                Player_matrix[i][j] -= 1
 
 def StopOnTheMine(GameField):
     for i in range(len(GameField)):
