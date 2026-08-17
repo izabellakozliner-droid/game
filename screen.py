b529c7246bd736fc6db57114d156f5102b1a2217
jbt <shacharab18@gmail.com>
2026-08-17 12:08:39 +0300

    dlfk

--- screen.py
+++ screen.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import sys
-import consts
+import consts,time
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -13,9 +13,19 @@ def flagSpawn():
 
 def solider_image():
     SOLIDER = pygame.image.load('soldier.png')
-    scaled_flag = pygame.transform.scale(SOLIDER, (consts.PLAYER_WIDTH, consts. PLAYER_HEIGHT))
-    screen.blit(scaled_flag, (60,80))
+    soldier = pygame.transform.scale(SOLIDER, (consts.PLAYER_WIDTH, consts. PLAYER_HEIGHT))
+    screen.blit(soldier, (200,100))
 
+def newScreen():
+    print("newScreen")
+    screen.fill((155,155,155))
+    pygame.display.update()
+
+def print_screen():
+    screen.fill((0,0,0))
+    flagSpawn()
+    solider_image()
+    pygame.display.update()
 
 
 
